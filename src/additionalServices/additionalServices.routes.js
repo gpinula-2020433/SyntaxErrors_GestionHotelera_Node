f3137a6107882181cb490978ec2e97de51d48baf
afigueroa-2023106 <afigueroa-2023106@kinal.edu.gt>
2025-04-22 21:31:10 -0600

    Se agrego el controller y el archivo routes de la entidad Servicios Adicionales

---
+++ src/additionalServices/additionalServices.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    getAllServices,
+    getServiceByID,
+    createService,
+    updateService,
+    deleteService
+} from './additionalServices.controller.js'
+import { validateJwt } from "../../middlewares/validate.jwt.js";
+
+const api = Router()
+
+api.get('/', [validateJwt], getAllServices)
+api.get('/:id', [validateJwt], getServiceByID)
+api.post('/', [validateJwt], createService)
+api.put('/:id', [validateJwt], updateService)
+api.delete('/:id', [validateJwt], deleteService)
+
+export default api
