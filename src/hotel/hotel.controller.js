09ceee6297baf732b13d396452bfdee106d09f17
kterraza-2023022 <kterraza-2023022@kinal.edu.gt>
2025-04-25 01:46:26 -0600

    Se corrige modelo Hotel y se arreglan errores

--- src/hotel/hotel.controller.js
+++ src/hotel/hotel.controller.js
@@ -1,55 +1,79 @@
-import Hotel from './hotel.model.js'
+import Hotel from "./hotel.model.js"
 
-export const getAllHotels = async (req, res) => {
-    const { limit = 10, skip = 0 } = req.query
-    try {
-        const hotels = await Hotel.find()
-            .skip(parseInt(skip))
-            .limit(parseInt(limit))
-
-        if (hotels.length === 0) {
-            return res.status(404).send({
-                success: false,
+export const getAllHotels = async (req, res)=> {
+    try{
+        const {limit = 10, skip =0}= req.query
+        const hotel = await Hotel.find()
+            .skip(skip)
+            .limit(limit)
+        if(hotel.length === 0)
+            return res.status(404).send(
+            {
+                succes: false,
                 message: 'Hotels not found'
-            })
-        }
 
-        return res.send({
-            success: true,
-            message: 'Hotels retrieved successfully',
-            total: hotels.length,
-            hotels
-        })
+            }
+        )
+        return res.send(
+            {
+                succes: true,
+                message: 'Hotels found',
+                hotel
+            }
+        )
     } catch (err) {
         console.error('General error', err)
-        return res.status(500).send({
-            success: false,
-            message: 'General error',
-            err
-        })
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error',
+                err
+            }
+        )
     }
 }
 
-export const getHotelById = async (req, res) => {
-    try {
-        const { id } = req.params
+export const getHotelById = async (req, res)=>{
+    try{
+        const{ id }= req.params
         const hotel = await Hotel.findById(id)
-            .populate('events')
-
         if (!hotel) {
             return res.status(404).send({
                 success: false,
                 message: 'Hotel not found'
             })
         }
-
         return res.send({
             success: true,
             message: 'Hotel found',
             hotel
         })
-    } catch (err) {
+
+    }catch (err){
         console.error('General error', err)
+        return res.status(500).send({
+            succes: false,
+            message: 'General error',
+            err
+        })
+    }
+}
+
+export const addHotel = async(req, res)=>{
+    try{
+        let data = req.body
+        let hotel = new Hotel(data)
+        await hotel.save()
+        return res.send(
+            {
+                success: true,
+                message: `Hotel successfully, ${hotel.name}`,
+                hotel
+            }
+        )
+        
+    }catch(err){
+        console.log('General error', err)
         return res.status(500).send({
             success: false,
             message: 'General error',
@@ -58,78 +82,62 @@ export const getHotelById = async (req, res) => {
     }
 }
 
-export const saveHotel = async (req, res) => {
-    const data = req.body
-    try {
-        const hotel = new Hotel(data)
-        await hotel.save()
-
-        return res.send({
-            success: true,
-            message: 'Hotel saved successfully',
-            hotel
-        })
-    } catch (err) {
-        console.error('General error', err)
-        return res.status(500).send({
-            success: false,
-            message: 'Error saving hotel',
-            err
-        })
-    }
-}
-
-export const updateHotel = async (req, res) => {
-    try {
-        const { id } = req.params
+export const updateHotel = async (req, res)=>{
+    try{
+        const {id} = req.params
         const data = req.body
-
-        const updatedHotel = await Hotel.findByIdAndUpdate(id, data, { new: true })
-
-        if (!updatedHotel) {
-            return res.status(404).send({
+        const update = await Hotel.findByIdAndUpdate(
+            id,
+            data,
+            {new: true}
+        )
+        if(!update)return res.status(404).send(
+            {
                 success: false,
                 message: 'Hotel not found'
-            })
-        }
+            }
+        )
+        return res.send(
+            {
+                success: true,
+                message: 'Hotel updated',
+                hotel: update
+            }
+        )
 
-        return res.send({
-            success: true,
-            message: 'Hotel updated successfully',
-            updatedHotel
-        })
-    } catch (err) {
-        console.error('General error', err)
-        return res.status(500).send({
-            success: false,
-            message: 'Error updating hotel',
-            err
-        })
+    }catch(err){
+        console.log('General error', err)
+        return res.status(500).send(
+            {
+                success: false,
+                messagge:'General error',
+                err
+            }
+        )
     }
 }
 
-export const deleteHotel = async (req, res) => {
+
+export const deleteHotel = async(req, res)=> {
     try {
-        const { id } = req.params
-        const deletedHotel = await Hotel.findByIdAndDelete(id)
-
-        if (!deletedHotel) {
-            return res.status(404).send({
-                success: false,
-                message: 'Hotel not found'
-            })
-        }
-
-        return res.send({
-            success: true,
-            message: 'Hotel deleted successfully'
-        })
-    } catch (err) {
-        console.error('General error', err)
-        return res.status(500).send({
+    const{id} = req.params
+    const hotel = await Hotel.findByIdAndDelete(id)
+    if (!hotel) {
+        return res.status(404).send({
             success: false,
-            message: 'Error deleting hotel',
-            err
+            message: 'Hotel not found'
         })
     }
+    return res.send({
+        success: true,
+        message: 'Deleted successfully'
+    })
+} catch (err) {
+    console.error('General error', err)
+    return res.status(500).send({
+        success: false,
+        message: 'General error',
+        err
+    })
+}
 }
--- src/hotel/hotel.model.js
+++ src/hotel/hotel.model.js
@@ -1,42 +1,43 @@
 import { Schema, model } from "mongoose";
 
 const hotelSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Hotel name is required']
-        },
-        address: {
-            type: String,
-            required: [true, 'Address is required']
-        },
-        category: {
-            type: String,
-            enum: ['1 STAR', '2 STARS', '3 STARS', '4 STARS', '5 STARS'],
-            required: [true, 'Category is required']
-        },
-        amenities: [{
-            type: String
-        }],
-        roomPricesByType: {
-            type: {
-                type: String,
-                required: [true, 'Room type is required']
-            },
-            price: {
-                type: Number,
-                required: [true, 'Price is required']
-            }
-        },
-        events: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Event'
-        }]
+  {
+    name: {
+      type: String,
+      required: [true, 'Hotel name is required']
     },
-    {
-        versionKey: false,
-        timestamps: true
+    address: {
+      type: String,
+      required: [true, 'Address is required']
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      maxlength: [200, 'Description must be no more than 200 characters']
+    },
+    phone: {
+      type: String,
+      required: [true, 'Phone is required'],
+      minlength: [8, 'Phone must be at least 8 digits'],
+      maxlength: [13, 'Phone must be no more than 13 digits']
+    },
+    category: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5], // Estrellas
+      required: [true, 'Category is required']
+    },
+    amenities: {
+      type: [String],
+      required: [true, 'Amenities are required']
+    },
+    imageHotel: {
+      type: String
     }
+  },
+  {
+    versionKey: false,
+    timestamps: true
+  }
 );
 
-export default model('Hotel', hotelSchema);
+export default model("Hotel", hotelSchema);
