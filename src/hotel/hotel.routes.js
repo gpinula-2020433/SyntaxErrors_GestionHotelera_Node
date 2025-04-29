4ecdb34de21024e78a09f1999b3d44673993b6f7
mpamal-2023046 <mpamal-2023046@kinal.edu.gt>
2025-04-28 23:47:33 -0600

    Se arreglan errores

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,19 +1,8 @@
-import { Router } from 'express'
-import {
-    getAllHotels,
-    getHotelById,
-    saveHotel,
-    updateHotel,
-    deleteHotel
-} from './hotel.controller.js'
-//import { validateJwt } from '../../middlewares/validate.jwt.js'
+import { Router } from "express";
+import { addHotel, getAllHotels } from "./hotel.controller.js";
 
 const api = Router()
-
-api.get('/', [validateJwt], getAllHotels)
-api.get('/:id', [validateJwt], getHotelById)
-api.post('/', [validateJwt], saveHotel)
-api.put('/:id', [validateJwt], updateHotel)
-api.delete('/:id', [validateJwt], deleteHotel)
+api.get('/',getAllHotels)
+api.post('/',addHotel)
 
 export default api
